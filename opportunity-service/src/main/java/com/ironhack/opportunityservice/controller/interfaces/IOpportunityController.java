d497eb953299b795a97f8daa2cef094bf38f0d06
Miguel Neves <miguel.a.seven@gmail.com>
2021-10-29 22:25:38 +0100

    added missing imports

--- opportunity-service/src/main/java/com/ironhack/opportunityservice/controller/interfaces/IOpportunityController.java
+++ opportunity-service/src/main/java/com/ironhack/opportunityservice/controller/interfaces/IOpportunityController.java
@@ -3,13 +3,15 @@ package com.ironhack.opportunityservice.controller.interfaces;
 import com.ironhack.opportunityservice.dto.ConversionReceipt;
 import com.ironhack.opportunityservice.dto.ConvertRequest;
 import com.ironhack.opportunityservice.dto.OpportunityDTO;
+import com.ironhack.opportunityservice.enums.Product;
+import com.ironhack.opportunityservice.enums.Status;
 
 import java.util.List;
 
 public interface IOpportunityController {
-    public List<OpportunityDTO> getOpportunities(Long id);
-    public ConversionReceipt convertLead(Long id, ConvertRequest convertRequest);
-    public void deleteOpportunity(Long id);
-    public OpportunityDTO updateStatus(Long id, Status status);
-    public List<OpportunityDTO> getByStatusAndProduct(Status status, Product product);
+    List<OpportunityDTO> getOpportunities(Long id);
+    ConversionReceipt convertLead(Long id, ConvertRequest convertRequest);
+    void deleteOpportunity(Long id);
+    OpportunityDTO updateStatus(Long id, Status status);
+    List<OpportunityDTO> getByStatusAndProduct(Status status, Product product);
 }
