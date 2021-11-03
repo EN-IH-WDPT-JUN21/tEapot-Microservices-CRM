ccea80ec680ddf97ab60eec4a43a506d7f2da352
patrykwieteska <patrykwieteskapw@gmail.com>
2021-11-03 07:39:46 +0100

    restored data loader in lead-service

--- LeadService/src/main/java/com/ironhack/leadservice/LeadServiceApplication.java
+++ LeadService/src/main/java/com/ironhack/leadservice/LeadServiceApplication.java
@@ -44,27 +44,27 @@ public class LeadServiceApplication {
 				.build();
 	}
 
-//	@Component
-//	public class SampleDataLoader implements CommandLineRunner {
-//		@Autowired
-//		LeadRepository leadRepository;
-//		@Override
-//		public void run(String... args) throws Exception {
-//			leadRepository.saveAll(List.of(
-//							new Lead("Oleg Smart", "0158158158", "Osmanrt@example.com", "Smart Solutions", 1L),
-//							new Lead("Adam Biggs", "0200200200", "abiggs@example.com", "BigBiggs", 1L),
-//							new Lead("Cedric Coleman", "0300300300", "ccoleman@example.com", "Cedar Coleman", 1L),
-//							new Lead("Daniel Dibbs", "0458789546", "ddibbs@example.com", "Dibb IT", 2L),
-//
-//							new Lead("Eric Edelmann", "0532578990", "eedelmann@example.com", "ElderMan", 2L),
-//							new Lead("Ferdinand Frog", "0121312122", "ffrog@example.com", "Smily Frog", 2L),
-//							new Lead("Greg Goll", "0321321321", "ggoll@example.com", "Goll", 2L),
-//
-//							new Lead("Henry Hickman", "0432432432", "hhickmann@example.com", "Hickory", 3L),
-//							new Lead("Izabella Ichtmann", "0543543543", "iichtmann@example.com", "Ichtiology Tech", 3L),
-//							new Lead("Jenny Jones", "0654654654", "jjones@example.com", "Jones&Jones", 3L)
-//					)
-//			);
-//		}
-//	}
+	@Component
+	public class SampleDataLoader implements CommandLineRunner {
+		@Autowired
+		LeadRepository leadRepository;
+		@Override
+		public void run(String... args) throws Exception {
+			leadRepository.saveAll(List.of(
+							new Lead("Oleg Smart", "0158158158", "Osmanrt@example.com", "Smart Solutions", 1L),
+							new Lead("Adam Biggs", "0200200200", "abiggs@example.com", "BigBiggs", 1L),
+							new Lead("Cedric Coleman", "0300300300", "ccoleman@example.com", "Cedar Coleman", 1L),
+							new Lead("Daniel Dibbs", "0458789546", "ddibbs@example.com", "Dibb IT", 2L),
+
+							new Lead("Eric Edelmann", "0532578990", "eedelmann@example.com", "ElderMan", 2L),
+							new Lead("Ferdinand Frog", "0121312122", "ffrog@example.com", "Smily Frog", 2L),
+							new Lead("Greg Goll", "0321321321", "ggoll@example.com", "Goll", 2L),
+
+							new Lead("Henry Hickman", "0432432432", "hhickmann@example.com", "Hickory", 3L),
+							new Lead("Izabella Ichtmann", "0543543543", "iichtmann@example.com", "Ichtiology Tech", 3L),
+							new Lead("Jenny Jones", "0654654654", "jjones@example.com", "Jones&Jones", 3L)
+					)
+			);
+		}
+	}
 }
